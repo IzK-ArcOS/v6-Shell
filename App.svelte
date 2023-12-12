ef49799053b23c3d5b8f640aa572517f8cbaf87c
Izaak Kuipers <izaak.kuipers@gmail.com>
2023-12-12 20:57:08 +0100

    Add Start menu base layout

--- App.svelte
+++ App.svelte
@@ -1,9 +1,18 @@
 <script lang="ts">
-  import { App } from "$types/app";
   import Shade from "./Components/Shade.svelte";
+  import StartMenu from "./Components/StartMenu.svelte";
   import "./css/main.css";
 
-  export let app: App;
+  //export let app: App;
 </script>
 
 <Shade />
+<div class="shell">
+  <div class="primary">
+    <StartMenu />
+    <!-- <ActionCenter/> -->
+  </div>
+  <div class="secondary">
+    <!-- <Taskbar/> -->
+  </div>
+</div>
---
+++ Components/StartMenu.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { UserDataStore } from "$ts/stores/user";
+  import { StartMenuOpened } from "../ts/stores";
+  import Bottom from "./StartMenu/Bottom.svelte";
+  import LeftPane from "./StartMenu/LeftPane.svelte";
+  import RightPane from "./StartMenu/RightPane.svelte";
+</script>
+
+{#if $UserDataStore}
+  <div
+    class="start-menu"
+    class:small={$UserDataStore.sh.start.small}
+    class:opened={$StartMenuOpened}
+  >
+    <div class="top">
+      <LeftPane />
+      <RightPane />
+    </div>
+    <Bottom />
+  </div>
+{/if}
