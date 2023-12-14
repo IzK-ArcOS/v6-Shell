367c1336a233e4ef57c5767d0066c7cfe32d1651
Izaak Kuipers <izaak.kuipers@gmail.com>
2023-12-14 19:30:13 +0100

    Implement start menu bottom pane

--- Components/StartMenu/Bottom.svelte
+++ Components/StartMenu/Bottom.svelte
@@ -1,1 +1,18 @@
-<div class="bottompane"></div>
+<script>
+  import ProfilePicture from "$lib/Components/ProfilePicture.svelte";
+  import { getProfilePicture } from "$ts/stores/pfp";
+  import { UserDataStore, UserName } from "$ts/stores/user";
+  import PowerOptions from "./Bottom/PowerOptions.svelte";
+
+  let pfp = "";
+
+  UserDataStore.subscribe((v) => {
+    pfp = getProfilePicture(v.acc.profilePicture);
+  });
+</script>
+
+<div class="bottompane">
+  <ProfilePicture src={pfp} height={20} />
+  <span>{$UserName}</span>
+  <PowerOptions />
+</div>
---
+++ Components/StartMenu/Bottom/PowerOptions.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import { spawnProcess } from "$ts/apps/process";
+
+  function search() {
+    return;
+  }
+
+  function settings() {
+    spawnProcess("SettingsApp");
+  }
+
+  function exit() {
+    spawnProcess("ExitApp");
+  }
+</script>
+
+<div class="options">
+  <button
+    class="material-icons-round"
+    on:click={search}
+    title="Search (Alt+Shift+S)">search</button
+  >
+  <button class="material-icons-round" on:click={settings}>settings</button>
+  <div class="sep" />
+  <button class="material-icons-round" on:click={exit}>
+    power_settings_new
+  </button>
+</div>
