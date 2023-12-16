fbc788591501fb87408afad6483bf53818b249f6
Izaak Kuipers <izaak.kuipers@gmail.com>
2023-12-16 22:50:23 +0100

    Update disposed check for taskbar opened apps

--- Components/Taskbar/OpenedApps.svelte
+++ Components/Taskbar/OpenedApps.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
   import { isPopulatable } from "$ts/apps/utils";
   import { ProcessStack } from "$ts/stores/process";
-  import { Process } from "$types/process";
+  import { ProcessMap } from "$types/process";
   import OpenedApp from "./OpenedApps/OpenedApp.svelte";
 
-  let processes: Map<number, Process | "disposed"> = new Map([]);
+  let processes: ProcessMap = new Map([]);
 
   ProcessStack.processes.subscribe((v) => (processes = v));
 </script>
 
 <div class="opened-apps">
   {#each [...processes] as [pid, proc]}
-    {#if proc != "disposed" && proc.app && isPopulatable(proc.app)}
+    {#if !proc._disposed && proc.app && isPopulatable(proc.app)}
       <OpenedApp {pid} app={proc.app} />
     {/if}
   {/each}
