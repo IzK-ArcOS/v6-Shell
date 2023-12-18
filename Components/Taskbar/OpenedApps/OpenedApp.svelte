831d09bfeb18a56f8de1703c5e4cfc530a1c2151
Izaak Kuipers <izaak.kuipers@gmail.com>
2023-12-18 16:30:43 +0100

    Add closing state to opened app buttons

--- Components/Taskbar/OpenedApps/OpenedApp.svelte
+++ Components/Taskbar/OpenedApps/OpenedApp.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
   import { focusedPid } from "$ts/stores/apps";
+  import { ProcessStack } from "$ts/stores/process";
   import { UserDataStore } from "$ts/stores/user";
   import { App } from "$types/app";
 
   export let pid: number;
   export let app: App;
+
+  let closing = false;
+
+  ProcessStack.closedPids.subscribe((v) => (closing = v.includes(pid)));
 
   function show() {
     $focusedPid = pid;
@@ -16,6 +21,7 @@
   on:click={show}
   class:activated={$focusedPid == pid}
   class:labels={$UserDataStore.sh.taskbar.labels}
+  class:closing
 >
   <img src={app.metadata.icon} alt={app.id} />
   {#if $UserDataStore.sh.taskbar.labels}
