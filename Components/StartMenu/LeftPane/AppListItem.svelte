ac70864e3d0b4d7d39245b204c1580f825496a81
Izaak Kuipers <izaak.kuipers@gmail.com>
2023-12-28 12:18:36 +0100

    Update app population

--- Components/StartMenu/LeftPane/AppListItem.svelte
+++ Components/StartMenu/LeftPane/AppListItem.svelte
@@ -1,16 +1,23 @@
 <script lang="ts">
   import { spawnApp } from "$ts/apps/spawn";
   import { isPopulatable } from "$ts/apps/utils";
+  import { UserDataStore } from "$ts/stores/user";
   import { App } from "$types/app";
 
   export let app: App;
+
+  let render = true;
+
+  UserDataStore.subscribe(() => {
+    render = isPopulatable(app);
+  });
 
   function open() {
     spawnApp(app.id);
   }
 </script>
 
-{#if isPopulatable(app)}
+{#if render}
   <button class="item" on:click={open}>
     <img src={app.metadata.icon} class="icon" alt={app.id} />
     {app.metadata.name}
--- Components/Taskbar/OpenedApps/OpenedApp.svelte
+++ Components/Taskbar/OpenedApps/OpenedApp.svelte
@@ -1,30 +1,51 @@
 <script lang="ts">
+  import { isPopulatable } from "$ts/apps";
   import { focusedPid } from "$ts/stores/apps";
   import { ProcessStack } from "$ts/stores/process";
   import { UserDataStore } from "$ts/stores/user";
+  import { sleep } from "$ts/util";
   import { App } from "$types/app";
 
   export let pid: number;
   export let app: App;
 
   let closing = false;
+  let render = true;
 
-  ProcessStack.closedPids.subscribe((v) => (closing = v.includes(pid)));
+  ProcessStack.closedPids.subscribe(update);
+
+  async function update(v: number[]) {
+    closing = v.includes(pid);
+
+    if (closing) {
+      await sleep(300);
+
+      render = false;
+    }
+  }
+
+  UserDataStore.subscribe(() => {
+    if (closing) return;
+
+    render = isPopulatable(app);
+  });
 
   function show() {
     $focusedPid = pid;
   }
 </script>
 
-<button
-  class="opened-app"
-  on:click={show}
-  class:activated={$focusedPid == pid}
-  class:labels={$UserDataStore.sh.taskbar.labels}
-  class:closing
->
-  <img src={app.metadata.icon} alt={app.id} />
-  {#if $UserDataStore.sh.taskbar.labels}
-    <span>{app.metadata.name}</span>
-  {/if}
-</button>
+{#if render}
+  <button
+    class="opened-app"
+    on:click={show}
+    class:activated={$focusedPid == pid}
+    class:labels={$UserDataStore.sh.taskbar.labels}
+    class:closing
+  >
+    <img src={app.metadata.icon} alt={app.id} />
+    {#if $UserDataStore.sh.taskbar.labels}
+      <span>{app.metadata.name}</span>
+    {/if}
+  </button>
+{/if}
