1edf56e7a7a130b126d4b4893ec50a6aaf37710b
Izaak Kuipers <izaak.kuipers@gmail.com>
2024-01-26 21:40:48 +0100

    Add system folders to start rightpane

--- Components/StartMenu/RightPane.svelte
+++ Components/StartMenu/RightPane.svelte
@@ -1,1 +1,10 @@
-<div class="rightpane"></div>
+<script>
+  import { SystemFolders } from "$apps/FileManager/ts/store";
+  import Folder from "./RightPane/Folder.svelte";
+</script>
+
+<div class="rightpane">
+  {#each SystemFolders as folder}
+    <Folder {folder} />
+  {/each}
+</div>
---
+++ Components/StartMenu/RightPane/Folder.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { SystemFolder } from "$apps/FileManager/ts/types";
+  import { spawnApp } from "$ts/apps";
+
+  export let folder: SystemFolder;
+
+  function goHere() {
+    spawnApp("FileManager", 0, [folder.path]);
+  }
+</script>
+
+<button class="folder" on:click={goHere}>
+  <img src={folder.icon} alt="" class="icon" />
+  <span>{folder.name}</span>
+</button>
