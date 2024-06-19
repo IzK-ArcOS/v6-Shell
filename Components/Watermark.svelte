47112df6f2439f2f6f5625ffd7217bb1a7a9cf06
Izaak Kuipers <izaak.kuipers@gmail.com>
2024-06-19 21:08:57 +0200

    Add newest log item to the watermark

--- Components/Watermark.svelte
+++ Components/Watermark.svelte
@@ -1,9 +1,18 @@
 <script lang="ts">
   import { SafeMode } from "$state/Desktop/ts/store";
+  import { LogStore } from "$ts/console";
   import { ArcOSVersion } from "$ts/env";
   import { ARCOS_BUILD, ARCOS_MODE } from "$ts/metadata";
   import { ConnectedServer } from "$ts/stores/server";
   import { UserName } from "$ts/stores/user";
+
+  let log = "-<br>-";
+
+  LogStore.subscribe(async (v) => {
+    if (!v.length) return;
+
+    log = `${v[v.length - 1].msg}`;
+  });
 </script>
 
 {#if ARCOS_MODE !== "release" || $SafeMode}
@@ -14,5 +23,7 @@
       ? "Secure"
       : "Open"})
     {#if $SafeMode}<br />Safe Mode Enabled{/if}
+    <hr />
+    {log}
   </div>
 {/if}
